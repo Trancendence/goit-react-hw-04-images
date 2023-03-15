b285b7bd6891ac75480a3495c4f80fd89ec87255
Transcendence <baydindenys@ukr.net>
2023-03-15 21:56:08 +0200

    update code
    update code

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,27 +1,27 @@
 import PropTypes from 'prop-types';
-
+import { useEffect } from 'react';
 import css from './Modal.module.css';
-import { Component } from 'react';
 
 
-export class Modal extends Component {
-  handleBackdrop = event => {
-    if (event.target === event.currentTarget) {
-      this.props.onImageClick('');
-    }
-  };
-  componentDidMount() {   
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {    
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onImageClick('');
-    }
-  };
-  render() {
+export const Modal = ({ onImageClick, largeImgUrl }) => {
+
+    useEffect( () => {
+      const handleKeyDown = e => {
+        if (e.code === 'Escape') {
+          onImageClick('');
+        }
+      };
+      window.addEventListener('keydown', handleKeyDown);
+      return () => window.removeEventListener('keydown', handleKeyDown);
+    }, [onImageClick])
+  
+    const handleBackdrop = event => {
+      if (event.target === event.currentTarget) {
+        onImageClick('');
+      }
+    };
+
+
     return (
       <div className={css.background} onClick={this.handleBackdrop}>
         <div className={css.modal}>
